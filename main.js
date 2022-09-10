20454997e940d34e49285b0927272a6932bbc79c
asasss3233 <113234397+asasss3233@users.noreply.github.com>
2022-09-10 20:24:17 +0300

    I'M CUUUUUUUUUUUULMINATING

--- main.js
+++ main.js
@@ -16,5 +16,13 @@ function set_root_var(name, value) {
 function load_module(url) {
     fetch(url).then((response) => response.text().then((data) => {
         console.log(data)
+        let data_without_scripts = data.split("\n")
+        let scripts = data_without_scripts.pop().split("&")
+        for (const i in scripts) {
+            let se = document.createElement("script")
+            se.src = scripts[i]
+            document.body.append(se)
+        }
+        document.getElementById("modular-content").innerHTML = data_without_scripts.join("")
     }))
 }
