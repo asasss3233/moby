a85b99f7b18974f0a517c6876c709671b01fac68
asasss3233 <113234397+asasss3233@users.noreply.github.com>
2022-09-10 17:15:59 +0300

    jj

--- sliding_overlay.js
+++ sliding_overlay.js
@@ -2,14 +2,21 @@ let opened_sliding_overlay_index = 0 // maximum can be 2 since there are only on
 
 function pop_sliding_overlay() {
     opened_sliding_overlay_index--
-    animate_css(document.querySelectorAll(".sliding-overlay")[opened_sliding_overlay_index], [{transform: "translateX(100vw)"}], {duration: 200})
+    animate_css(document.querySelectorAll(".sliding-overlay")[opened_sliding_overlay_index], [{transform: "translateX(100vw)"}], {duration: 200, easing: "cubic-bezier(0.22, 1, 0.36, 1)"})
 }
 
-function open_sliding_overlay(logo_url, channel_name, subscribers_amount, sliding_tabs_labels, description) {
+function switch_sliding_overlay_tab(sliding_tabs_labels, content) {
+    document.querySelectorAll(".sliding-tabs p")[opened_sliding_overlay_index].innerText = sliding_tabs_labels[0]
+    document.querySelectorAll(".sliding-tabs p")[opened_sliding_overlay_index + 1].innerText = sliding_tabs_labels[1]
+
+    document.querySelectorAll(".tab-content")[opened_sliding_overlay_index].innerHTML = content
+}
+
+function open_sliding_overlay(logo_url, channel_name, subscribers_amount, sliding_tabs_labels, content) {
     document.querySelectorAll(".brief-channel div img")[opened_sliding_overlay_index].src = logo_url
     document.querySelectorAll(".brief-channel h3")[opened_sliding_overlay_index].innerText = channel_name
     document.querySelectorAll(".brief-channel p")[opened_sliding_overlay_index].innerText = subscribers_amount
-    document.querySelectorAll(".tab-content p")[opened_sliding_overlay_index].innerText = description
+    document.querySelectorAll(".tab-content")[opened_sliding_overlay_index].innerHTML = content
 
     document.querySelectorAll(".sliding-tabs p")[opened_sliding_overlay_index].innerText = sliding_tabs_labels[0]
     document.querySelectorAll(".sliding-tabs p")[opened_sliding_overlay_index + 1].innerText = sliding_tabs_labels[1]
