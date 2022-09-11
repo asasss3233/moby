3984a40f44917cfe0b5988ff3ece309f3016cc79
asasss3233 <113234397+asasss3233@users.noreply.github.com>
2022-09-11 10:04:35 +0300

    kavun

--- sliding_overlay.js
+++ sliding_overlay.js
@@ -3,20 +3,23 @@ window.close_sliding_overlay = (el) => {
     animate_css(el, [{transform: "translateX(100vw)"}], {duration: 200, easing: "cubic-bezier(0.22, 1, 0.36, 1)"})
 }
 
-window.open_sliding_overlay_v1 = (logo_url, channel_name, subscribers_amount, description, cards) => {
-    document.querySelectorAll(".brief-channel div img")[0].src = logo_url
-    document.querySelectorAll(".brief-channel h3")[0].innerText = channel_name
-    document.querySelectorAll(".brief-channel p")[0].innerText = subscribers_amount
-    document.querySelectorAll(".tab-content p")[0].innerText = description
+window.open_sliding_overlay_v1 = (logo_url, channel_name, subscribers_amount, description, cards, id) => {
+    let el = document.getElementById(id)
+    el.children[0].children[1].children[0].children[0].src = logo_url
+    el.children[0].children[1].children[1].innerText = channel_name
+    el.children[0].children[1].children[2].innerText = subscribers_amount
+    el.children[0].children[2].children[1].innerText = description
 
-    animate_css(document.querySelectorAll(".sliding-overlay")[0], [{transform: "translateX(0vw)"}], {duration: 200, delay: 50})
+    animate_css(el, [{transform: "translateX(0vw)"}], {duration: 200, delay: 50})
 }
 
-window.open_sliding_overlay_v2 = (logo_url, event_name, event_date, description, cards) => {
-    document.querySelectorAll(".sliding-overlay-wrapper .cover img")[0].src = logo_url
-    document.querySelectorAll(".sliding-overlay-wrapper .heading")[0].innerText = event_name
-    document.querySelectorAll(".sliding-overlay-wrapper .date")[0].innerText = event_date
-    document.querySelectorAll(".sliding-overlay-wrapper .tab-content p")[1].innerText = description
+window.open_sliding_overlay_v2 = (logo_url, event_name, event_date, description, cards, id) => {
+    let el = document.getElementById(id)
+    console.log(el.children[0])
+    el.children[0].children[1].children[0].src = logo_url
+    el.children[0].children[2].innerText = event_name
+    el.children[0].children[3].innerText = event_date
+    el.children[0].children[4].children[1].innerText = description
 
-    animate_css(document.querySelectorAll(".sliding-overlay")[1], [{transform: "translateX(0vw)"}], {duration: 200, delay: 50})
+    animate_css(el, [{transform: "translateX(0vw)"}], {duration: 200, delay: 50})
 }
