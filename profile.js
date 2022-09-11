e27de63f421f721387ef5c243f0c8139f37be04a
asasss3233 <113234397+asasss3233@users.noreply.github.com>
2022-09-11 09:07:35 +0300

    kavun

--- profile.js
+++ profile.js
@@ -2,3 +2,14 @@ document.querySelector(".profile-wrapper .brief-channel img").src = "/loc-images
 document.querySelector(".profile-wrapper .brief-channel h3").innerText = "Тигран Кашапов"
 document.querySelector(".profile-wrapper .brief-channel p").innerText = "320 уровень"
 document.querySelector(".profile-wrapper .description").innerText = "Нажмите, чтобы редактировать описание профиля"
+
+// ip/register/Tigran_Kashapov/pass343word/ijsadiojjio
+
+// ip/register/<str:username>/<str:password>/<str:description>
+// render_template_string("true")
+
+// ip/login/<str:username>/<str:password>
+// render_template_string("true")
+
+// ip/get_events/<str:username>/
+// render_template_string("[...]")
